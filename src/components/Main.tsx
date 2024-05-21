b66ce2fb76aa416f510670fd5dc5c37a1cebd444
Pedroo07 <pedrohenriquemoalive@gmail.com>
2024-05-21 11:25:54 -0300

    importando e utilizando lib lowlight

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,32 @@
-import { useEditor,EditorContent } from "@tiptap/react"
+import { EditorContent, useEditor } from '@tiptap/react'
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
 import StarterKit from "@tiptap/starter-kit"
+import {common, createLowlight} from 'lowlight'
+const lowlight = createLowlight(common)
+import ts from 'highlight.js/lib/languages/typescript'
+
+import 'highlight.js/styles/monokai.css'
+
+lowlight.register({ts})
 
 const Main = () => {
     const editor = useEditor({
-        extensions: [StarterKit],
+        extensions: [StarterKit,
+            CodeBlockLowlight.configure({
+                lowlight,
+            }),
+        ],
         content: '<p>Hello World</p>',
-        editorProps:{
+        editorProps: {
             attributes: {
                 class: 'outline-none'
             }
         }
     })
-  return (
-    <EditorContent className="max-w-[700px] mx-auto pt-16 prose" editor={editor}
-    />
-  )
+    return (
+        <EditorContent className="max-w-[700px] mx-auto pt-16 prose" editor={editor}
+        />
+    )
 }
 
 export default Main
