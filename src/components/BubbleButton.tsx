fb4c5c540fe21c8a3d8bb579cbb3183a2283c81b
Pedroo07 <pedrohenriquemoalive@gmail.com>
2024-05-22 08:49:55 -0300

    Criando e estilizando botoes

---
+++ src/components/BubbleButton.tsx
@@ -0,0 +1,15 @@
+import {  ComponentProps,ReactNode } from 'react'
+
+export interface BubbleButtonProps extends ComponentProps<'button'> {
+    children: ReactNode
+}
+
+const BubbleButton = (props: BubbleButtonProps) => {
+  return (
+    <button className='p-2 text-zinc-800 text-base flex items-center gap-1.5 font-medium data-[active=true]:text-violet-400' {...props}>
+
+    </button>
+  )
+}
+
+export default BubbleButton
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,10 +2,13 @@ import { EditorContent, useEditor } from '@tiptap/react'
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
 import StarterKit from "@tiptap/starter-kit"
 import {common, createLowlight} from 'lowlight'
+import { BubbleMenu } from '@tiptap/react'
+import {RxFontBold,RxFontItalic,RxStrikethrough,RxCode} from 'react-icons/rx'
 const lowlight = createLowlight(common)
 import ts from 'highlight.js/lib/languages/typescript'
 
 import 'highlight.js/styles/monokai.css'
+import BubbleButton from './BubbleButton'
 
 lowlight.register({ts})
 
@@ -24,9 +27,18 @@ const Main = () => {
         }
     })
     return (
+        <>
         <EditorContent className="max-w-[700px] mx-auto pt-16 prose" editor={editor}
         />
+        {editor && (<BubbleMenu className='bg-zinc-200 shadow-lg shadow-black/20 border-zinc-600 overflow-hidden rounded-lg flex divide-x-2 divide-zinc-6 00 ' editor={editor}>
+            <BubbleButton onClick={() => editor.chain().focus().toggleBold().run()} data-active={editor.isActive('bold')}><RxFontBold className='w-5 h-5'/></BubbleButton>
+            <BubbleButton onClick={() => editor.chain().focus().toggleItalic().run()} data-active={editor.isActive('italic')}><RxFontItalic className='w-5 h-5'/></BubbleButton>
+            <BubbleButton onClick={() => editor.chain().focus().toggleStrike().run()} data-active={editor.isActive('strike')}><RxStrikethrough className='w-5 h-5'/></BubbleButton>
+            <BubbleButton onClick={() => editor.chain().focus().toggleCode().run()} data-active={editor.isActive('code')}><RxCode className='w-5 h-5'/></BubbleButton>
+        </BubbleMenu>)}
+        
+        </>
     )
 }
 
-export default Main
+export default Main 
